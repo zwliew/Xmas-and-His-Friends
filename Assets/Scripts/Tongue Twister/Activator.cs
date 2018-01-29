40d0762df2c6d9975537619ae9113a8c1a721e5c
josephaureliano <hanjiale1998@outlook.com>
2018-01-29 15:41:54 +0800

    raokouling game

--- Assets/Scripts/Tongue Twister/Activator.cs
+++ Assets/Scripts/Tongue Twister/Activator.cs
@@ -8,16 +8,42 @@ public class Activator : MonoBehaviour {
     public KeyCode key;
     bool active = false;
     GameObject note;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(key)&&active)//Input.GetTouch(0).position.x>-6&& Input.GetTouch(0).position.x<-3&&Input.GetTouch(0).position.y>-4&& Input.GetTouch(0).position.x < -2 && active) //detect area touched
+    SpriteRenderer sr;
+    Color old;
+    public bool createMode;
+    public GameObject n;
+    // Use this for initialization
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void Start () {
+        old = sr.color;
+        PlayerPrefs.SetInt("score", 0);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (createMode)
         {
-            Destroy(note);
+            if (Input.GetKeyDown(key)) {
+                Instantiate(n, transform.position, Quaternion.identity);
+}
+
+        }
+        else
+        {
+            if (Input.GetKeyDown(key))
+            {
+                StartCoroutine(pressed());
+            }
+            if (Input.GetKeyDown(key) && active)//Input.GetTouch(0).position.x>-6&& Input.GetTouch(0).position.x<-3&&Input.GetTouch(0).position.y>-4&& Input.GetTouch(0).position.x < -2 && active) //detect area touched
+            {
+                Destroy(note);
+                addScore();
+                active = false;
+            }
         }
 	}
 
@@ -34,6 +60,20 @@ public class Activator : MonoBehaviour {
     void OnTriggerExit2D(Collider2D col)
     {
         active = false;
+    }
+
+    void addScore()
+    {
+        PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score") + 1);
+
+    }
+
+    IEnumerator pressed()
+    {
+        old = sr.color;
+        sr.color = new Color(0, 0, 0);
+        yield return new WaitForSeconds(0.02f);
+        sr.color = old;
     }
 }
 
---
+++ Assets/Scripts/Tongue Twister/Note.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+    Rigidbody2D rb;
+    public float speed;
+    Vector2 centre = new Vector2(0,0);
+    // Use this for initialization
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start () {
+        rb.velocity = new Vector2(0, -speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(rb.GetRelativePoint(centre).y< -6)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
