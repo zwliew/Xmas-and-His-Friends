453b99f95015b1913422a717b3567463dd8a05a8
josephaureliano <hanjiale1998@outlook.com>
2018-02-01 08:09:07 +0800

    trying to recover the character slicer game

---
+++ Assets/Scripts/character/LineFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineFollow : MonoBehaviour {
+
+    LineRenderer line;
+    private int vertexCount;
+    private bool mousedown = false;
+	// Use this for initialization
+	void Start () {
+        line = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousedown = true;
+        }
+        if (mousedown)
+        {
+            line.positionCount = vertexCount + 1;
+            Vector3 mousepos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15f));
+            line.SetPosition(vertexCount, mousepos);
+            vertexCount++;
+            BoxCollider2D collider = gameObject.AddComponent<BoxCollider2D>();
+            collider.transform.position = line.transform.position;
+            collider.size = new Vector2(0.2f, 0.2f);
+
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            vertexCount = 0;
+            line.positionCount = 0;
+            mousedown = false;
+        }
+	}
+}
