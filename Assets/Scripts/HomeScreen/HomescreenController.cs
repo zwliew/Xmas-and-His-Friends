7b52a7a865b9914514c95092298246b08a17443d
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-03-08 17:49:49 +0800

    Try to set pos. of equipped items when loading HomeScreen

---
+++ Assets/Scripts/HomeScreen/HomescreenController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomescreenController : MonoBehaviour {
+
+    private PlayerDataController playerDataController;
+    private List<string> equippedItemNames;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerDataController = GameObject.FindGameObjectWithTag("Persistent")
+            .GetComponent<PlayerDataController>();
+        PlayerData playerData = playerDataController.GetPlayerData();
+        equippedItemNames = playerData.equippedItems;
+        foreach (string itemName in equippedItemNames)
+        {
+            GameObject equippedGameObject = equippedGameObject.Find(itemName);
+            equippedGameObject.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
