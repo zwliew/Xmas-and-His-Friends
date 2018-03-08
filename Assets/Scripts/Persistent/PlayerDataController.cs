e75cd43c7853dd412e746f4789213a24df37fbf1
annnnnnnnnnie <2330610648@qq.com>
2018-03-08 22:59:37 +0800

    Merge

--- Assets/Scripts/Persistent/PlayerDataController.cs
+++ Assets/Scripts/Persistent/PlayerDataController.cs
@@ -12,9 +12,9 @@ public class PlayerDataController : MonoBehaviour
 {
     private PlayerData playerData;
 
-    void Start()
+    void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+		DontDestroyOnLoad (gameObject);
 		PlayerPrefs.DeleteAll ();
         LoadPlayerData();
     }
