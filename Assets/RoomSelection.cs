54ee570946b3d54d09ba490467cd5cd4a4a5247d
annnnnnnnnnie <2330610648@qq.com>
2018-03-16 23:32:23 +0800

    Added some UI

---
+++ Assets/RoomSelection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomSelection : MonoBehaviour {
+	public Button previousBtn;
+	public Button nextBtn;
+	public Button roomOneBtn;
+	public Button roomTwoBtn;
+	public Image fadeImage;
+	private int currentRoomNumber;
+
+	void Start(){
+		currentRoomNumber = 0;
+		Refresh ();
+	}
+
+	private void Refresh(){
+		fadeImage.GetComponent<Animator> ().SetTrigger ("Exit");
+		switch (currentRoomNumber) {
+		case 0:
+			roomOneBtn.gameObject.SetActive (true);
+			roomTwoBtn.gameObject.SetActive (false);
+			Debug.Log ("Now showing roomOne");
+			break;
+		case 1:
+			roomOneBtn.gameObject.SetActive (false);
+			roomTwoBtn.gameObject.SetActive (true);
+			Debug.Log ("Now showing roomTwo");
+			break;
+		default:
+			Debug.Log ("Now showing default");
+			break;
+		}
+	}
+
+	public void PreviousRoom(){
+		currentRoomNumber = 0;
+		Refresh ();
+	}
+
+	public void NextRoom(){
+		currentRoomNumber = 1;
+		Refresh ();
+	}
+}
---
+++ Assets/Scripts/HomeScreen/FurnitureInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureInteraction : MonoBehaviour {
+
+	private RaycastHit hitInfo;
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hitInfo, 300f, 1 << LayerMask.NameToLayer ("Road"))) {
+				Debug.Log (hitInfo.collider.gameObject.name);
+				AudioSource audio =	hitInfo.collider.gameObject.GetComponent<AudioSource> ();
+				if (audio != null) {
+					audio.Play();
+				}
+			}
+		}
+
+
+	}
+}
