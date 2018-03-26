efb2361b18b6af439be2fed8af73395954830982
josephaureliano <hanjiale1998@outlook.com>
2018-03-26 21:17:09 +0800

    added a maze scene
    depending on progress, it may be developed

---
+++ Assets/Scripts/maze/MazeDataController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeDataController : MonoBehaviour {
+
+    int test = 12;
+	void Start () {
+        distribute(test);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private int[] distribute(int number)
+    {
+        int extra = number - 5;
+        int[] numberPerRow = { 0,0,0,0,0 };
+        int pointer = 0;
+        while (extra > 0)
+        {
+            int curRow = Random.Range(0, 4);
+            if(numberPerRow[curRow] < 4)
+            {
+                numberPerRow[curRow] += 1;
+                extra -= 1;
+            }
+        }
+        
+        for(int i = 0; i < 6; i++)
+        {
+            pointer = numberPerRow[i];
+
+
+        }
+        foreach(int no in numberPerRow)
+        {
+            Debug.Log(no);
+        }
+        Debug.Log("distribution complete: " + Time.deltaTime);
+        return numberPerRow;
+    }
+}
