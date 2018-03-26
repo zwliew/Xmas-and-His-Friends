ac7a2e2640fbdcf6a790fa8afda82b253827a4fc
josephaureliano <hanjiale1998@outlook.com>
2018-03-26 23:15:43 +0800

    dysfunctional navmesh
    I have tried to bake a navmesh but seems something is wrong with the sizes of the model, and the model si not moving despite applying the script that i adapted from the tutorial

---
+++ Assets/Scripts/HomeScreen/DirectedAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DirectedAgent : MonoBehaviour {
+
+    private NavMeshAgent agent;
+	// Use this for initialization
+	void Awake () {
+        agent = GetComponent<NavMeshAgent>();
+
+	}
+	
+    public void MoveToLocation(Vector3 targetLocation)
+    {
+        agent.SetDestination(targetLocation);
+        agent.isStopped = false;
+    }
+}
