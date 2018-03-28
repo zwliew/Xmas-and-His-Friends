71c16c3c3c4e7cae3f000ddf9f094ba6bba0beaa
josephaureliano <hanjiale1998@outlook.com>
2018-03-28 21:45:22 +0800

    woyaocommit

--- Assets/Scripts/HomeScreen/FurnitureSoundController.cs
+++ Assets/Scripts/HomeScreen/FurnitureSoundController.cs
@@ -21,13 +21,18 @@ public class FurnitureSoundController : MonoBehaviour {
 
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hitInfo;
-		if (Physics.Raycast (ray, out hitInfo, 300f, 1 << LayerMask.NameToLayer("Road"))) {
-			// Load AudioClip from Sounds/Furniture/<audioClipName>
+		if (Physics.Raycast (ray, out hitInfo, 300f, 1 << LayerMask.NameToLayer("Furniture"))) {
+            // Load AudioClip from Sounds/Furniture/<audioClipName>
+            Debug.Log("TRying to play sound: " + hitInfo.collider.gameObject.name);
 			AudioClip clip = Resources.Load<AudioClip>(
 				"Sounds/Furniture/" + hitInfo.collider.gameObject.name);
-			if (source != null && clip != null) {
-				source.PlayOneShot(clip);
-			}
+            if (source != null && clip != null)
+            {
+                source.PlayOneShot(clip);
+            }
+            else {
+                Debug.Log("didnot get the clip");
+            }
 		}
 	}
 }
