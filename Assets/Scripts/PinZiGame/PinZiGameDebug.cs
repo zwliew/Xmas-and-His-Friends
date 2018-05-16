df27efaa5ece5b1c7108b48f2e9b0c00c6c14ea8
annnnnnnnnnie <2330610648@qq.com>
2018-05-16 14:58:47 +0800

    Update PinZiGameDebug.cs

--- Assets/Scripts/PinZiGame/PinZiGameDebug.cs
+++ Assets/Scripts/PinZiGame/PinZiGameDebug.cs
@@ -1,1 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class PinZiGameDebug : Monobehaviour{
+    
+    public DataController datacontroller;
+    public GameObject[] 
+    private float xAxis;
+    private float yAxis;
+    private final float zAxis = 0f;
+    
+    private Word[] allWords;
+    
+    public void Start(){
+        datacontroller.Initialize();
+        allWords = datacontroller.GetAllWords();
+    }
+    
+    public void Update(){
+        if(Input.GetMouseBotton(0)){
+            Debug.log("Displaying all words");
+            DisplayAllWords();
+        }
+    }
+    
+    private void DisplayAllWords(){
+        Debug.log("There are " + words.size.ToString() + " words to be displayed");
+        
+    }
+    
+}
