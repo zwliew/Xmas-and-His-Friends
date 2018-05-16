4c47606c9830ec0acfd25b39a8abf4b3dec5679b
annnnnnnnnnie <2330610648@qq.com>
2018-05-16 15:19:17 +0800

    Update PinZiGameDebug.cs

--- Assets/Scripts/PinZiGame/PinZiGameDebug.cs
+++ Assets/Scripts/PinZiGame/PinZiGameDebug.cs
@@ -12,6 +12,8 @@ public class PinZiGameDebug : Monobehaviour{
     private final float zAxis = 0f;
     
     private Word[] allWords;
+    private Texture2D texture2DSides;
+    private Texture2D texture2DAns;
     
     public void Start(){
         datacontroller.Initialize();
@@ -27,36 +29,40 @@ public class PinZiGameDebug : Monobehaviour{
     
     private void DisplayAllWords(){
         Debug.log("There are " + words.size.ToString() + " words to be displayed");
+	    
         for(int i = 0; i<100; i++){
-            Word word = words[i];
-            string[] sides = word.sides;
-	    texture2DSides = new Texture2D[sides.Length];
-            for (int i = 0; i < sides.Length; i++) {
-			string strTexturePath = "PinZiPianPang/" + sides [i].ToString ();
-		    	//Debug.Log ("Loading " + (i+1) + " " +strTexturePath);
-		    	texture2DSides[i] =	Resources.Load (strTexturePath) as Texture2D;
-	    		texture2DSidesSelected[i] =	Resources.Load (strTexturePath+"r") as Texture2D;
-    		//Debug.Log ("Loaded " + texture2DSides [i].name);
-	    	//Debug.Log ("Loading... " + (i+1) + "/" + sides.Length);
-    		yield return new WaitForFixedUpdate ();
-    	    }
-            string strCorrectTexturePath = "PinZiPianPang/" + word.name.ToString ();
+	    	xAxis = 10 * i;
+	    	yAixs = 0;
+		
+        	Word word = words[i];
+			string[] sides = word.sides;
+			texture2DSides = new Texture2D[sides.Length];
+		
+        	for (int i = 0; i < sides.Length; i++) {
+				string strTexturePath = "PinZiPianPang/" + sides [i].ToString ();
+				//Debug.Log ("Loading " + (i+1) + " " +strTexturePath);
+				texture2DSides[i] =	Resources.Load (strTexturePath) as Texture2D;
+				//Debug.Log ("Loaded " + texture2DSides [i].name);
+				//Debug.Log ("Loading... " + (i+1) + "/" + sides.Length);
+				yield return new WaitForFixedUpdate ();
+			}
+	
+			string strCorrectTexturePath = "PinZiPianPang/" + word.name.ToString ();
 	    	texture2DAns =	Resources.Load (strCorrectTexturePath) as Texture2D;
-            for (int i = 0; i < sides.Length; i++) {
-			    priPrefabPianPangs[i] = GameObjectUtility.customInstantiate (prefabPianPangs [i], goPlaceHolders[i].transform.position, goPlaceHolders[i].transform.rotation);
-		    	priPrefabPianPangs [i].transform.parent = goTetra.transform; 
-		    	//Debug.Log ("Getting pinZiScript");
-	    		PinZiPP pinZiScript = priPrefabPianPangs[i].GetComponent<PinZiPP> ();
-	    		//Debug.Log ("Initializing");
-                pinZiScript.Initialize ();
-    			//Debug.Log ("Setting texture");
-    			pinZiScript.SetDisplay (texture2DSides [i], texture2DSidesSelected[i]);
-
-	    		pinZiScript.sidename = texture2DSides [i].name;
-		    	//Debug.Log ("-----------Assigned: " + (i + 1) + "/" + sides.Length + "------------");
-			    yield return new WaitForFixedUpdate ();
-		    }
-        }
-    }
-    
+		
+			for (int i = 0; i < sides.Length; i++) {
+				priPrefabPianPangs[i] = GameObjectUtility.customInstantiate (prefabPianPangs [i], goPlaceHolders[i].transform.position, goPlaceHolders[i].transform.rotation);
+				priPrefabPianPangs[i].transform.position = new Vector3(xAxis, yAxis,zAxis);
+				//Debug.Log ("Getting pinZiScript");
+				PinZiPP pinZiScript = priPrefabPianPangs[i].GetComponent<PinZiPP> ();
+				//Debug.Log ("Initializing");
+				pinZiScript.Initialize ();
+				//Debug.Log ("Setting texture");
+				pinZiScript.SetDisplay (texture2DSides [i], texture2DSidesSelected[i]);
+				pinZiScript.sidename = texture2DSides [i].name;
+				//Debug.Log ("-----------Assigned: " + (i + 1) + "/" + sides.Length + "------------");
+				yield return new WaitForFixedUpdate ();
+	    	}
+    	}
+	}
 }
