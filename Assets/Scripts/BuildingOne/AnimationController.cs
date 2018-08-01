ebadd46c2edbbe54e63ab2d606b44ccefa379212
annnnnnnnnnie <2330610648@qq.com>
2018-08-01 15:29:28 +0800

    Add comments

--- Assets/Scripts/BuildingOne/AnimationController.cs
+++ Assets/Scripts/BuildingOne/AnimationController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//NOT IN USE
 public class AnimationController : MonoBehaviour {
 	[HideInInspector]
 	public string holdFlag;
